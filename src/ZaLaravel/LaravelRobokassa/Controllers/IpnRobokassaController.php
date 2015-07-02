c78c917a2c8beb62ca49a8ff6382021a529590b7
Lyovkin <anatoliymolinari@gmail.com>
2015-07-02 21:57:10 +0300

    modify controller

--- src/ZaLaravel/LaravelRobokassa/Controllers/IpnRobokassaController.php
+++ src/ZaLaravel/LaravelRobokassa/Controllers/IpnRobokassaController.php
@@ -3,9 +3,9 @@
 namespace ZaLaravel\LaravelRobokassa\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Log;
+use App\Models\User;
 
 /**
  * Class IpnRobokassaController
@@ -15,13 +15,25 @@ class IpnRobokassaController extends Controller{
 
     public function getResult(Request $request)
     {
+        $user = $request->user()->id;
         $out_sum = $request->get('OutSum');
         $inv_id = $request->get('InvId');
-        $sign = $request->get('SignatureValue');
+        $password2 = config('roboconfig.testPassword2');
 
-        Log::info('Полученная сумма: '.$out_sum, 'номер  счета в магазине: '.$inv_id,
-            'контр. сумма: '.$sign);
-
-        return new Response('Ok' . $inv_id);
+        if(Payment::where('uid', '=', $inv_id) && Payment::where('balance', '=', $out_sum))
+        {
+            $payment = Payment::where('uid', '=', $inv_id)->first();
+            $payment->status = 1;
+            $payment->update();
+            $addBalanceToUser = User::find($user);
+            $addBalanceToUser->balance += $out_sum;
+            $addBalanceToUser->update();
+        }else
+        {
+            \Session::flash('message', 'Подпись или сумма не совпадает, повторите попытку.');
+            return redirect()->action('ProfileController@index');
+        }
+        \Session::flash('message', "Ваш баланс пополнен на $out_sum рублей.");
+        return redirect()->action('ProfileController@index');
     }
 }
